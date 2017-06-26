dc56a76754dc00c20bc48a867e77fdc76e9483d6
Payten Giles <payten.giles@gmail.com>
2017-06-27 09:47:18 +1000

    Remove duplicate labels from subnotes

--- public/models/squash_large_notes.rb
+++ public/models/squash_large_notes.rb
@@ -9,33 +9,15 @@ module SquashLargeNotes
 
     # now all labels that match for this note type will be merged sans label
     super
+
+    # finally remove duplicate labels from sub notes
+    if notes_hash[type] && notes_hash[type]['subnotes']
+      ASUtils.wrap(notes_hash[type]['subnotes']).each do |sub|
+        if notes_hash[type]['label'] == sub['_inline_label']
+          sub.delete('_inline_label')
+        end
+      end
+    end
   end
 
 end
-
-# def merge_notes_by_type(notes_hash, type, note_struct)
-#   # If we haven't seen a note of this type yet, just take the first one
-#   if !notes_hash.has_key? type
-#     notes_hash[type] = note_struct
-#     return
-#   end
-# 
-#   # Otherwise, do a merge
-#   if notes_hash[type]['label'].blank?
-#     # Our first label
-#     notes_hash[type]['label'] = note_struct['label']
-#   elsif notes_hash[type]['label'] != note_struct['label']
-#     # Add a secondary label as an inline label
-#     note_struct['note_text']= "<span class='inline-label'>#{note_struct['label']}</span> #{note_struct['note_text']}"
-#   end
-# 
-#   notes_hash[type]['note_text'] = "#{notes_hash[type]['note_text']}<br/><br/> #{note_struct['note_text']}"
-# 
-#   if note_struct.has_key?('subnotes')
-#     notes_hash[type]['subnotes'] ||= []
-#     notes_hash[type]['subnotes'] = notes_hash[type]['subnotes'] + note_struct['subnotes'].map{|sub|
-#       sub['_inline_label'] = note_struct['label']
-#       sub
-#     }
-#   end
-# end
