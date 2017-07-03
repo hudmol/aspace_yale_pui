8b4f0eb450748a937ea266bfa022293366c8238a
Payten Giles <payten.giles@gmail.com>
2017-07-04 09:22:03 +1000

    Retain zero extents and only show their container_summary, physical_details and dimensions

--- public/models/record_ext.rb
+++ public/models/record_ext.rb
@@ -2,11 +2,30 @@ Record.class_eval do
 
   alias_method :parse_extents_pre_aspace_yale_pui, :parse_extents
   def parse_extents
-    json['extents'] = ASUtils.wrap(json['extents']).select {|ext|
-      ext['number'] != '0'
-    }
+    zero_extents = []
+    ASUtils.wrap(json['extents']).each_with_index do |ext, i|
+      if  ext['number'] == '0'
+        type = I18n.t("enumerations.extent_extent_type.#{ext['extent_type']}", default: ext['extent_type'])
+        display = ext['container_summary'] || type # show type if container_summary is blank
+        display << I18n.t('extent_phys_details',:deets => ext['physical_details']) unless  ext['physical_details'].blank?
+        display << I18n.t('extent_dims', :dimensions => ext['dimensions']) unless  ext['dimensions'].blank?
 
-    parse_extents_pre_aspace_yale_pui
+        zero_extents << {
+          'display' => display,
+          '_inherited' => ext.dig('_inherited'),
+          '_index' => i, # capture the extent's index so we can retain the subrecord order
+        }
+      end
+    end
+
+    result = parse_extents_pre_aspace_yale_pui
+
+    # plug in the custom display string for zero extents
+    zero_extents.each do |ext|
+      result[ext['_index']] = ext
+    end
+
+    result
   end
 
 end
